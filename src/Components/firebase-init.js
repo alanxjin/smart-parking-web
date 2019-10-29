658ac2fde11f91f37a78f18685111c98d4b0b409
mjiang24 <mjiang24@illinois.edu>
2019-10-29 11:30:02 -0500

    fix bug

---
+++ src/Components/firebase-init.js
@@ -0,0 +1,19 @@
+import app from 'firebase/app';
+import fb from 'firebase';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAZ7kVfUkG6D-04t4Io8F2OwzZwNRhtb5M",
+    authDomain: "ismart-parking-meter.firebaseapp.com",
+    databaseURL: "https://ismart-parking-meter.firebaseio.com",
+    projectId: "ismart-parking-meter",
+    storageBucket: "ismart-parking-meter.appspot.com",
+    messagingSenderId: "860777835594",
+    appId: "1:860777835594:web:b8064ab97e365a9a840ab3",
+    measurementId: "G-9Y20CKPN4K"
+};
+
+app.initializeApp(firebaseConfig);
+var db = fb.firestore();
+export { db };
