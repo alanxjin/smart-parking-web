578c498dea485bf70f4ec2876a77fb9c00a2b725
Alan Jin <xilunjin7@gmail.com>
2019-12-02 21:06:11 -0600

    Add router

--- src/App.js
+++ src/App.js
@@ -1,41 +1,59 @@
 import React , { Component } from 'react';
 import './App.css';
-import Menu from './Components/Menu';
+import {Menu} from 'semantic-ui-react';
 import Home from './Components/Home';
 import MyMeter from './Components/MyMeter';
 import Settings from './Components/Settings';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+ } from "react-router-dom";
+
+import firebase from 'firebase';
+
+
+// Configure FirebaseUI.
+const uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+    signInSuccessUrl: '/signedIn',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ]
+  };
 
 class App extends Component{
-    state = { 
-        activeMenuItem: 'home', // home, my meter, settings
-        time: 0 // This will be fetched from database 
-    }
-    componentDidMount(){
-        this.interval = setInterval(this.increaseTime, 1000)
-    }
-    increaseTime = () => { 
-        this.setState((preState)=> ({time: preState.time +1}))
-    }
-
-    handleMenuItemClick = (e, { name }) => this.setState({ activeMenuItem: name })
-    
-    getContent = (activeMenuItem) => {
-        switch(activeMenuItem) {
-            case 'home':
-                return <Home/>
-            case 'my meter':
-                return <MyMeter time={this.state.time}/>
-            default:
-                return <Settings/>;
-          }
-    }
-
+  
     render() {
-        const {activeMenuItem} = this.state;
+      
         return (
             <div id="App">
-            <Menu attached activeMenuItem={activeMenuItem} handleMenuItemClick={this.handleMenuItemClick}/>
-            {this.getContent(activeMenuItem)}
+                <Router>
+                    <div id="nav">
+                    <Menu inverted pointing attached>
+                        <Menu.Item as={Link} name='home' to='/home'/>
+                        <Menu.Item as={Link} name='my meter' to='/my_meter'/>
+                        <Menu.Item as={Link} name='settings' to='/settings'/>
+                    </Menu>
+                    </div>
+                    <Switch>
+                        <Route path="/home">
+                            <Home/>
+                        </Route>
+                        <Route path="/my_meter">
+                            <MyMeter/>
+                        </Route>
+                        <Route path="/settings">
+                            <Settings/>
+                        </Route>
+                    </Switch>
+                </Router>
             </div>
         )
     }
